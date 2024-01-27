b507091b383edaca9c58baeef4266ea9218274ed
Vijayakumar-Periyasamy <vijayrose1053@gmail.com>
2024-01-27 05:46:05 +0530

    fixed

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
-
 const sequelize = new Sequelize(
   process.env.DATABASE_NAME,
   process.env.DATABASE_USER,
   process.env.DATABASE_PASS,
   {
-    host: "localhost",
+    host: "parvathyhomecare.ctyme06sa802.ap-south-1.rds.amazonaws.com",
     dialect: "mysql",
     operatorsAliases: false,
     define: {
