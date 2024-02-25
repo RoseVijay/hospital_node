037800474e51303829a07135317b7ce9ccf35f24
Vijayakumar-Periyasamy <vijayakumar.periyasamy@platform3solutions.com>
2024-02-25 13:13:48 +0530

    fixed

--- routes/razorpay.js
+++ routes/razorpay.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const {createOrder,payment} = require("../controllers/razorpay");
+const {createOrder,payment,sendOTP} = require("../controllers/razorpay");
 
 router.post("/createOrder", createOrder);
 router.post("/payment", payment);
+router.post("/sendOTP", sendOTP);
 module.exports = router;
